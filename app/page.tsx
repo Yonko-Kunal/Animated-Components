76daea1feb3718520cfe86b519632154a0b84269
Yonko-Kunal <128897093+Yonko-Kunal@users.noreply.github.com>
2026-05-03 18:47:09 +0530

    Fix: Responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,23 @@ import React from "react";
 import AnimatedHeader from "@/components/AnimatedHeader/AnimatedHeader";
 
 export default function Home() {
+	const textStyle =
+		"2xl:text-8xl md:text-7xl text-5xl font-bold text-center 2xl:w-[30%] md:w-[40%] w-[70%]";
 	return (
 		<div>
 			<div className="bg-[#2b0350] text-[#c67de1] flex items-center justify-center h-screen w-full">
 				<AnimatedHeader>
-					<h1 className="text-8xl font-bold text-center w-[30%]">
-						Batman is the real GOAT
-					</h1>
+					<h1 className={`${textStyle}`}>Batman is the real GOAT</h1>
 				</AnimatedHeader>
 			</div>
-			<div className="flex items-center justify-center h-screen w-full">
+			<div className="flex items-center justify-center h-screen w-full bg-black text-white">
 				<AnimatedHeader animateOnScroll>
-					<h1 className="text-8xl font-bold text-center w-[30%]">
-						Batman is the real GOAT
-					</h1>
+					<h1 className={`${textStyle}`}>Batman is the real GOAT</h1>
 				</AnimatedHeader>
 			</div>
 			<div className="bg-[#2f4f07] text-[#9ee781] flex items-center justify-center h-screen w-full">
 				<AnimatedHeader scrub>
-					<h1 className="text-6xl font-bold text-center w-[70%]">
-						Batman is the real GOAT
-					</h1>
+					<h1 className={`${textStyle}`}>Batman is the real GOAT</h1>
 				</AnimatedHeader>
 			</div>
 		</div>
